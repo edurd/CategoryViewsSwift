4fc8ef6f059fbd9f63c11fca79ed8b0dcebcb0d8
Eduardo Hoyos <edu.hoyos92@gmail.com>
2017-06-07 02:14:47 -0500

    Initial Commit

---
+++ Test CollectionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test CollectionView
+//
+//  Created by Eduardo Hoyos on 6/7/17.
+//  Copyright © 2017 Jcodee SAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
